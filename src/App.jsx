9038ea17c282f67589f4ac8f085194f0ed126586
MAURO METZ DA SILVA <0103786@acad.redes-ienh.com.br>
2025-07-03 20:19:47 -0300

    sim

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,38 @@
 import './App.css'
+import { FaCat } from "react-icons/fa";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <button className="bg-green-600 md:bg-red-500 rounded-xl p-2" type="button">
-        CLICA AQUI, EU SEI QUE VOCÊ QUER CLICAR
-      </button>
-      <form action="https://getform.io/f/bvrydqmb" method="POST">
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <input type="text" name="message" />
-        {/* add hidden Honeypot input to prevent spams */}
-        <input type="hidden" name="_gotcha" style={{ display: "none !important" }} />
-        {/* checkbox handle */}
-        <input
-          type="checkbox"
-          name="subscribe"
-          defaultValue="yes"
-          defaultChecked=""
-        />
-        <input type="hidden" name="subscribe" defaultValue="no" />
-        {/* radio button handle */}
-        <input type="radio" name="gender" defaultValue="male" defaultChecked="" />
-        <input type="radio" name="gender" defaultValue="female" />
-        <input type="radio" name="gender" defaultValue="other" />
-        {/* select field handle */}
-        <select name="work-experience">
-          <option value="one-year">0-1 years</option>
-          <option value="one-five-years">1-5 years</option>
-        </select>
-        <button type="submit">Send</button>
-      </form>
-
-
-      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
-        <button class="infinite-rotate-on-hover backdrop-blur-md bg-white/30 border border-white/50 shadow-lg text-black font-semibold px-6 py-3 rounded-2xl hover:bg-white/50 hover:shadow-xl transition-all duration-300">
-          Não clique ali. Clique Aqui
-        </button>
-      </a>
+      <div className='bg-white w-screen h-15 flex items-center justify-center gap-2'>
+        <FaCat className='text-purple-900 text-xl' />
+        <div className='font-bold text-purple-900 text-xl'>DudaSys</div>
+        <FaCat className='text-purple-900 text-xl' />
+      </div>
+      {/* alerta de gambiarra */}
+      <div className='bg-purple-400 w-full h-[42.7vw] justify-items-center pt-48'>
+        <div className='bg-white w-64 h-48 justify-items-center rounded-sm pt-6'>
+          <div className='font-semibold'>Entrar na sua conta</div>
+          <form action="https://getform.io/f/bkknzkeb" method="POST">
+            <input
+              className="border border-gray-500 rounded-[3px] w-48 h-8 mt-2 ml-8 focus:border-purple-500 focus:outline-none"
+              type="email"
+              name="email"
+              placeholder=" Digite o email"
+            />
+            <input
+              className="border border-gray-500 rounded-[3px] w-48 h-8 mt-0.5 ml-8 focus:border-purple-500 focus:outline-none"
+              type="password"
+              name="password"
+              placeholder=" Digite a senha"
+            />
+            <div className='h-100px w-100px'>
+              <button className='bg-purple-600 hover:bg-purple-500 text-white rounded-[3px] w-48 h-8 mt-2 ml-8' type="submit">Entrar</button>
+            </div>
+          </form>
+        </div>
+      </div>
 
     </>
   )
